0b4df201670f61a30646ae0fd40a794c21459610
Raymon Skjørten Hansen <rsha@dips.no>
2024-03-22 22:49:41 +0100

    Close on esc

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
