d45ae1d2e1950bd283825e6cd9fb093227c118ae
Raymon Skjørten Hansen <rsha@dips.no>
2024-03-22 22:47:55 +0100

    Greet world... graphically

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/MesloLGLNerdFont-Regular.ttf");
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: 60.0,
+        color: Color::WHITE,
+    };
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(Text2dBundle {
+        text: Text {
+            sections: vec![TextSection::new(
+                format!("Hello, World!"),
+                text_style,
+            )],
+            ..Default::default()
+        },
+        transform: Transform::from_translation(250. * Vec3::Y),
+        ..default()
+    });
 }
